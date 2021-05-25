590dc69f297d37be80dc885cde56500d4da0fd6c
Javier <sosafuch@gmail.com>
2021-05-25 17:37:41 -0300

    resolve: cors

--- api/src/index.js
+++ api/src/index.js
@@ -1,17 +1,20 @@
-require('dotenv').config()
-const express = require('express')
-const morgan = require('morgan')
+require('dotenv').config();
+const express = require('express');
+const morgan = require('morgan');
 const bodyParser = require('body-parser');
-const app = express()
-const port = process.env.PORT || 3001
-const routes = require('./routes')
+const app = express();
+const routes = require('./routes');
+const port = process.env.PORT || 3001;
+const cors = require('cors');
 
-app.use(morgan('dev'))
+app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({limit: '50mb', extended: true, parameterLimit: 50000}));
 app.use(bodyParser.json({limit: '50mb'}));
+app.use(cors({ credentials: true }));
 
 app.use('/', routes);
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+  console.log(`WisbooAPI running on port ${port}`);
+  console.log(`created by Tomas0011 siganme bebes <3`);
 })
